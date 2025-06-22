b8049d253462945c7b64e5dd2e2e9caaecd407fa
Rysuca <178133294+Rysuca@users.noreply.github.com>
2025-06-23 00:00:00 +0300

    upd main file

--- smart_todo.py
+++ smart_todo.py
@@ -2,3 +2,11 @@ import json
 import os
 from datetime import datetime, timedelta
 from collections import defaultdict
+
+
+class SmartTodoList:
+    def __init__(self, filename="tasks.json"):
+        self.filename = filename
+        self.tasks = []
+        self.load_tasks()
+        
